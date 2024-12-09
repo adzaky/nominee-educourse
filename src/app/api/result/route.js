01d7599ad0a877f7a73fdd22cad28613218af1a2
adzaky <muhadlim17@gmail.com>
2024-12-09 23:29:02 +0700

    refactor: lower case nominee names before calculation, return title case, and remove prefix for categories other than mostkind mentor

--- src/app/api/result/route.js
+++ src/app/api/result/route.js
@@ -1,4 +1,5 @@
 import { connectToDatabase } from "@/lib/db";
+import { removePrefix, toTitleCase } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
 export async function GET() {
@@ -27,15 +28,23 @@ export async function GET() {
       const voteCount = {};
 
       nominees.forEach((nomination) => {
-        const nominee = nomination[category];
+        let nominee = nomination[category];
         if (nominee) {
-          voteCount[nominee] = (voteCount[nominee] || 0) + 1;
+          if (category !== "mostkind_mentor") {
+            nominee = removePrefix(nominee);
+          }
+
+          const lowerNominee = nominee.toLowerCase();
+          voteCount[lowerNominee] = (voteCount[lowerNominee] || 0) + 1;
         }
       });
 
       const sortedVotes = Object.entries(voteCount).sort((a, b) => b[1] - a[1]);
 
-      return sortedVotes.map(([name, count]) => ({ name, count }));
+      return sortedVotes.map(([lowerName, count]) => ({
+        name: toTitleCase(lowerName),
+        count,
+      }));
     };
 
     const results = {};
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,18 @@
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+export function toTitleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+export function removePrefix(name) {
+  return name.replace(/^(Mr|Ms)\s+/i, "");
 }
